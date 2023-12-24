ed71754aad47f7e7a28d84918b1f7238758557aa
for2codespace <148958459+for2codespace@users.noreply.github.com>
2023-12-24 08:18:55 +0000

    u

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 function App() {
+  const subdomain = window.location.host.split('.')[0];
+
   return (
     <div className="App">
-      <h1>Hello, детки!</h1>
+      <h1>Hello, детки! {subdomain}</h1>
     </div>
   );
 }
