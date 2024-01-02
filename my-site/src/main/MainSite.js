c65682655945400b1488dfc210fec68c3b5a5ca5
for2codespace <148958459+for2codespace@users.noreply.github.com>
2024-01-02 17:21:23 +0000

    updates

--- my-site/src/main/MainSite.js
+++ my-site/src/main/MainSite.js
@@ -1,6 +1,10 @@
 export default function MainSite () {
 
     return (
-        <h1>MainSite</h1>
+    <>
+        <h1>Сайтик Глеба Блебовича</h1>
+        <hr></hr>
+        <a href="messenger.exp-of-betrayal">Будущий месенджер</a>
+    </>
     )
 }
