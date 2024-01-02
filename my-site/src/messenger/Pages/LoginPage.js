4caaf6f51ed677495b578c00e2df809694b7ed15
for2codespace <148958459+for2codespace@users.noreply.github.com>
2024-01-02 15:46:26 +0000

    updates

--- my-site/src/messenger/Pages/LoginPage.js
+++ my-site/src/messenger/Pages/LoginPage.js
@@ -5,6 +5,8 @@ export default function LoginPage () {
     return (
         <div className="LoginForm">
             <center><div className='Title'>Вход</div></center>
+            <div className='Input' placeholder="E-mail" contentEditable></div>
+            <div className='Input' placeholder="Пароль" contentEditable></div>
         </div>
     )
 }
