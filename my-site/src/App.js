1c6f5673d7ac4cf9780471600a230114745763ac
for2codespace <148958459+for2codespace@users.noreply.github.com>
2024-01-02 07:50:35 +0000

    updates

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 
-function App() {
-  const subdomain = window.location.host.split('.')[0];
+import Messanger from './messanger/Messanger.js';
+import MainSite from './main/MainSite.js';
 
+function App() {
+  const domen = "exp-of-betrayal"
   return (
-    <div className="App">
-      <h1>Hello, детки! {subdomain}</h1>
-    </div>
+    <>
+      <DomenRoute domain={domen} element={<MainSite />} />
+      <DomenRoute domain={"messanger." + domen} element={<Messanger />} />
+    </>
   );
+}
+
+
+function DomenRoute ({ domain, element }) {
+  const subdomain = window.location.host.replace('.ru', '');
+
+  if (subdomain === domain)
+    return element
 }
 
 export default App;
