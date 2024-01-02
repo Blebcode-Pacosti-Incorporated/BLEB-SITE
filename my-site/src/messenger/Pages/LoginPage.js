9ef291b16ea0c8597f727a26ebd1143a557bf237
for2codespace <148958459+for2codespace@users.noreply.github.com>
2024-01-02 16:35:17 +0000

    updates

--- my-site/src/messenger/Pages/LoginPage.js
+++ my-site/src/messenger/Pages/LoginPage.js
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import './css/LoginPage.css';
 
 export default function LoginPage () {
@@ -5,9 +6,26 @@ export default function LoginPage () {
     return (
         <div className="LoginForm">
             <center><div className='Title'>Вход</div></center>
-            <div className='Input' placeholder="E-mail" contentEditable></div>
-            <div className='Input' placeholder="Пароль" contentEditable></div>
+            <div className='Input' placeholder="E-mail" contentEditable />
+            <div className='Input' placeholder="Пароль" contentEditable />
             <center><div className='Button'>Войти</div></center>
+            <div className='Divider' />
+            <linkpad>
+                <Link>Забыли пароль?</Link>
+                <Link>Регистрация</Link>
+            </linkpad>
+        </div>
+    )
+}
+
+function Link ({ href, children }) {
+    const navigate = useNavigate()
+    return (
+        <div 
+            className='Link' 
+            onClick={ () => {navigate(href)} }
+        >
+            {children}
         </div>
     )
 }
